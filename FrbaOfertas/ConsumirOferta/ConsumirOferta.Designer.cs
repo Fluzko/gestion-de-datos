8756d4def9128e1a694a13cbc455b6fb02481067
Mauro Cicerchia <maurocicerchia98@gmail.com>
2019-11-02 18:28:02 -0300

    limpio pantalla

--- FrbaOfertas/ConsumirOferta/ConsumirOferta.Designer.cs
+++ FrbaOfertas/ConsumirOferta/ConsumirOferta.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FrbaOfertas.ComprarOferta
 {
-    partial class ComprarOferta
+    partial class ConsumirOferta
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.Label lblProveedor;
             this.gridOfertas = new System.Windows.Forms.DataGridView();
-            this.grpFiltros = new System.Windows.Forms.GroupBox();
-            this.txtDescripcion = new System.Windows.Forms.TextBox();
-            this.lblDescripcion = new System.Windows.Forms.Label();
-            this.txtPrecioMax = new System.Windows.Forms.TextBox();
-            this.lblPrecioMax = new System.Windows.Forms.Label();
-            this.txtPrecioMin = new System.Windows.Forms.TextBox();
-            this.lblPrecioMin = new System.Windows.Forms.Label();
-            this.txtProveedor = new System.Windows.Forms.TextBox();
-            this.btnFiltrar = new System.Windows.Forms.Button();
-            this.btnComprar = new System.Windows.Forms.Button();
-            this.lblCantidad = new System.Windows.Forms.Label();
-            this.numCantidad = new System.Windows.Forms.NumericUpDown();
-            lblProveedor = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.gridOfertas)).BeginInit();
-            this.grpFiltros.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblProveedor
-            // 
-            lblProveedor.AutoSize = true;
-            lblProveedor.Location = new System.Drawing.Point(6, 34);
-            lblProveedor.Name = "lblProveedor";
-            lblProveedor.Size = new System.Drawing.Size(74, 17);
-            lblProveedor.TabIndex = 0;
-            lblProveedor.Text = "Proveedor";
             // 
             // gridOfertas
             // 
@@ -71,161 +46,21 @@
             this.gridOfertas.TabIndex = 0;
             this.gridOfertas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridOfertas_CellClick);
             // 
-            // grpFiltros
-            // 
-            this.grpFiltros.Controls.Add(this.txtDescripcion);
-            this.grpFiltros.Controls.Add(this.lblDescripcion);
-            this.grpFiltros.Controls.Add(this.txtPrecioMax);
-            this.grpFiltros.Controls.Add(this.lblPrecioMax);
-            this.grpFiltros.Controls.Add(this.txtPrecioMin);
-            this.grpFiltros.Controls.Add(this.lblPrecioMin);
-            this.grpFiltros.Controls.Add(this.txtProveedor);
-            this.grpFiltros.Controls.Add(lblProveedor);
-            this.grpFiltros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.grpFiltros.Location = new System.Drawing.Point(12, 12);
-            this.grpFiltros.Name = "grpFiltros";
-            this.grpFiltros.Size = new System.Drawing.Size(807, 117);
-            this.grpFiltros.TabIndex = 2;
-            this.grpFiltros.TabStop = false;
-            this.grpFiltros.Text = "Filtros";
-            // 
-            // txtDescripcion
-            // 
-            this.txtDescripcion.Location = new System.Drawing.Point(91, 60);
-            this.txtDescripcion.Name = "txtDescripcion";
-            this.txtDescripcion.Size = new System.Drawing.Size(295, 23);
-            this.txtDescripcion.TabIndex = 7;
-            // 
-            // lblDescripcion
-            // 
-            this.lblDescripcion.AutoSize = true;
-            this.lblDescripcion.Location = new System.Drawing.Point(6, 63);
-            this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(82, 17);
-            this.lblDescripcion.TabIndex = 6;
-            this.lblDescripcion.Text = "Descripcion";
-            // 
-            // txtPrecioMax
-            // 
-            this.txtPrecioMax.Location = new System.Drawing.Point(506, 60);
-            this.txtPrecioMax.Name = "txtPrecioMax";
-            this.txtPrecioMax.Size = new System.Drawing.Size(295, 23);
-            this.txtPrecioMax.TabIndex = 5;
-            this.txtPrecioMax.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecioMax_KeyPress);
-            // 
-            // lblPrecioMax
-            // 
-            this.lblPrecioMax.AutoSize = true;
-            this.lblPrecioMax.Location = new System.Drawing.Point(405, 63);
-            this.lblPrecioMax.Name = "lblPrecioMax";
-            this.lblPrecioMax.Size = new System.Drawing.Size(99, 17);
-            this.lblPrecioMax.TabIndex = 4;
-            this.lblPrecioMax.Text = "Precio Máximo";
-            // 
-            // txtPrecioMin
-            // 
-            this.txtPrecioMin.Location = new System.Drawing.Point(506, 31);
-            this.txtPrecioMin.Name = "txtPrecioMin";
-            this.txtPrecioMin.Size = new System.Drawing.Size(295, 23);
-            this.txtPrecioMin.TabIndex = 3;
-            this.txtPrecioMin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecioMin_KeyPress);
-            // 
-            // lblPrecioMin
-            // 
-            this.lblPrecioMin.AutoSize = true;
-            this.lblPrecioMin.Location = new System.Drawing.Point(405, 34);
-            this.lblPrecioMin.Name = "lblPrecioMin";
-            this.lblPrecioMin.Size = new System.Drawing.Size(96, 17);
-            this.lblPrecioMin.TabIndex = 2;
-            this.lblPrecioMin.Text = "Precio Minimo";
-            // 
-            // txtProveedor
-            // 
-            this.txtProveedor.Location = new System.Drawing.Point(91, 31);
-            this.txtProveedor.Name = "txtProveedor";
-            this.txtProveedor.Size = new System.Drawing.Size(295, 23);
-            this.txtProveedor.TabIndex = 1;
-            // 
-            // btnFiltrar
-            // 
-            this.btnFiltrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnFiltrar.Location = new System.Drawing.Point(21, 135);
-            this.btnFiltrar.Name = "btnFiltrar";
-            this.btnFiltrar.Size = new System.Drawing.Size(79, 30);
-            this.btnFiltrar.TabIndex = 3;
-            this.btnFiltrar.Text = "Filtrar";
-            this.btnFiltrar.UseVisualStyleBackColor = true;
-            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
-            // 
-            // btnComprar
-            // 
-            this.btnComprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnComprar.Location = new System.Drawing.Point(726, 135);
-            this.btnComprar.Name = "btnComprar";
-            this.btnComprar.Size = new System.Drawing.Size(79, 30);
-            this.btnComprar.TabIndex = 4;
-            this.btnComprar.Text = "Comprar";
-            this.btnComprar.UseVisualStyleBackColor = true;
-            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
-            // 
-            // lblCantidad
-            // 
-            this.lblCantidad.AutoSize = true;
-            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblCantidad.Location = new System.Drawing.Point(526, 140);
-            this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(68, 17);
-            this.lblCantidad.TabIndex = 8;
-            this.lblCantidad.Text = "Cantidad:";
-            // 
-            // numCantidad
-            // 
-            this.numCantidad.Location = new System.Drawing.Point(600, 140);
-            this.numCantidad.Maximum = new decimal(new int[] {
-            0,
-            0,
-            0,
-            0});
-            this.numCantidad.Name = "numCantidad";
-            this.numCantidad.Size = new System.Drawing.Size(120, 20);
-            this.numCantidad.TabIndex = 9;
-            // 
             // ComprarOferta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(831, 576);
-            this.Controls.Add(this.numCantidad);
-            this.Controls.Add(this.lblCantidad);
-            this.Controls.Add(this.btnComprar);
-            this.Controls.Add(this.btnFiltrar);
-            this.Controls.Add(this.grpFiltros);
             this.Controls.Add(this.gridOfertas);
             this.Name = "ComprarOferta";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.gridOfertas)).EndInit();
-            this.grpFiltros.ResumeLayout(false);
-            this.grpFiltros.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView gridOfertas;
-        private System.Windows.Forms.GroupBox grpFiltros;
-        private System.Windows.Forms.TextBox txtPrecioMin;
-        private System.Windows.Forms.Label lblPrecioMin;
-        private System.Windows.Forms.TextBox txtProveedor;
-        private System.Windows.Forms.TextBox txtPrecioMax;
-        private System.Windows.Forms.Label lblPrecioMax;
-        private System.Windows.Forms.TextBox txtDescripcion;
-        private System.Windows.Forms.Label lblDescripcion;
-        private System.Windows.Forms.Button btnFiltrar;
-        private System.Windows.Forms.Button btnComprar;
-        private System.Windows.Forms.Label lblCantidad;
-        private System.Windows.Forms.NumericUpDown numCantidad;
     }
 }
--- FrbaOfertas/ConsumirOferta/ConsumirOferta.cs
+++ FrbaOfertas/ConsumirOferta/ConsumirOferta.cs
@@ -10,108 +10,17 @@ using System.Windows.Forms;
 
 namespace FrbaOfertas.ComprarOferta
 {
-    public partial class ComprarOferta : Form
+    public partial class ConsumirOferta : Form
     {
-        private Modelos.Oferta current;
-
-        public ComprarOferta()
+        public ConsumirOferta()
         {
             InitializeComponent();
             Decoracion.Reorganizar(this);
-            List<Modelos.Oferta> ofertas = DB_Ofertas.getOfertas();
-            showOfertas(ofertas);
-        }
-
-        private void showOfertas(List<Modelos.Oferta> ofertas)
-        {
-            gridOfertas.DataSource = ofertas;
-            //gridOfertas.AutoResizeColumns();
-            gridOfertas.Rows[0].Selected = true;
-            current = getRow(0);
-            int maxCliente = ((Modelos.Oferta)gridOfertas.Rows[0].DataBoundItem).MaxPorCliente;
-            int cantDisponible = ((Modelos.Oferta)gridOfertas.Rows[0].DataBoundItem).CantDisponible;
-            if (maxCliente < cantDisponible)
-                numCantidad.Maximum = maxCliente;
-            else
-                numCantidad.Maximum = cantDisponible;
         }
 
         private void gridOfertas_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            int i = e.RowIndex;
-            if (i >= 0)
-            {
-                current = getRow(i);
-                numCantidad.Maximum = current.MaxPorCliente;
-            }
-        }
 
-        private Modelos.Oferta getRow(int i)
-        {
-            DataGridViewRow selectedRow = gridOfertas.Rows[i];
-            return (Modelos.Oferta)selectedRow.DataBoundItem;
-        }
-
-        private void btnComprar_Click(object sender, EventArgs e)
-        {
-            if (numCantidad.Value == 0)
-            {
-                MessageBox.Show("Seleccione una cantidad mayor a 0", "Compra Ofertas", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            DialogResult res = MessageBox.Show("¿Desea comprar " + numCantidad.Value + " de estas ofertas?", "Compra Ofertas", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-            
-            if (res == DialogResult.OK)
-            {
-                DB_Ofertas.comprarOferta(Session.getUser(), current, (int)numCantidad.Value);
-            }
-
-            List<Modelos.Oferta> ofertas = DB_Ofertas.getOfertas();
-            showOfertas(ofertas);
-        }
-
-        private void btnFiltrar_Click(object sender, EventArgs e)
-        {
-            List<Modelos.Oferta> ofertas;
-            String[] searchables = loadSearchInputs();
-
-            if (searchables.All(searchable => string.IsNullOrEmpty(searchable) || string.IsNullOrWhiteSpace(searchable)))
-            {
-                ofertas = DB_Ofertas.getOfertas();
-                showOfertas(ofertas);
-                return;
-            }
-
-            ofertas = DB_Ofertas.getOfertasWithCondition(txtProveedor.Text, txtDescripcion.Text, txtPrecioMin.Text, txtPrecioMax.Text);
-            if (ofertas == null)
-            {
-                MessageBox.Show("No hay ofertas que coincidan con su busqueda", "Compra Ofertas", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                return;
-            }
-            showOfertas(ofertas);
-            return;
-        }
-
-        private String[] loadSearchInputs()
-        {
-            String[] i = {
-                            this.txtProveedor.Text,
-                            this.txtDescripcion.Text,
-                            this.txtPrecioMin.Text,
-                            this.txtPrecioMax.Text
-                         };
-            return i;
-        }
-
-        private void txtPrecioMin_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            Validar.numerico(e);
-        }
-
-        private void txtPrecioMax_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            Validar.numerico(e);
         }
     }
 }
