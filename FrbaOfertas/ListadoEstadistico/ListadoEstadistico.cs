619e39d98ba32a675db750b6f227588fe7807f65
nvent97 <37757410+nvent97@users.noreply.github.com>
2019-11-02 21:30:15 -0300

    UI carga credito y listado estadistico

--- FrbaOfertas/ListadoEstadistico/ListadoEstadistico.cs
+++ FrbaOfertas/ListadoEstadistico/ListadoEstadistico.cs
@@ -8,13 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FrbaOfertas.CragaCredito
+namespace FrbaOfertas.ListadoEstadistico
 {
-    public partial class Form1 : Form
+    public partial class ListadoEstadistico : Form
     {
-        public Form1()
+        public ListadoEstadistico()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
